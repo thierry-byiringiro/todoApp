ebe7ee0dc6c8ed456a8805a2f75fee6fc2c295f2
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-19 20:48:06 +0200

    added loaders and action to the routers

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,43 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Body,{action as bodyAction,loader as bodyLoader} from "./pages/Body";
+import Body, { action as bodyAction, loader as bodyLoader } from "./pages/Body";
 import Layout from "./components/Layout";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Update, {
+  loader as updateLoader,
+  action as updateAction,
+} from "./pages/Update";
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route element={<Layout/>}> 
-    <Route index path="/" element={<Body/>} action={bodyAction} loader={bodyLoader}/>
-  </Route>
-))
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout />}>
+      <Route
+        index
+        path="/"
+        element={<Body />}
+        action={bodyAction}
+        loader={bodyLoader}
+      />
+      <Route
+        path=":id"
+        element={<Update />}
+        action={updateAction}
+        loader={updateLoader}
+      />
+    </Route>,
+  ),
+);
 function App() {
-
   return (
     <>
-      <div className="w-100 h-100 mt-20  flex flex-col mx-auto items-center border">
-        <RouterProvider router={router}/>
+      <div className="w-100 h-fit mt-20  flex flex-col  mx-auto items-center border">
+        <RouterProvider router={router} />
       </div>
     </>
   );

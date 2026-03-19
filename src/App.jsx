76ab788ed9f6e3f160fb2256dad42ede3509f262
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-19 13:19:25 +0200

    added the routing and added actions, loaders

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Body,{action as bodyAction,loader as bodyLoader} from "./pages/Body";
+import Layout from "./components/Layout";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route element={<Layout/>}> 
+    <Route index path="/" element={<Body/>} action={bodyAction} loader={bodyLoader}/>
+  </Route>
+))
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="w-100 h-100 mt-20  flex flex-col mx-auto items-center border">
+        <RouterProvider router={router}/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

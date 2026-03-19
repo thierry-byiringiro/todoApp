2564ea3e41688f9f4beace5e136f0057c46fc651
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-19 20:45:45 +0200

    updating the todo through the actions

---
+++ src/pages/Update.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { redirect, useLoaderData } from "react-router-dom";
+import { Form } from "react-router-dom";
+
+export function loader({ params }) {
+  return params.id;
+}
+
+export async function action({ request,params }) {
+    const formData = await request.formData();
+    const newTodo = formData.get("task");
+    const currentTodos = JSON.parse(localStorage.getItem("task") || "[]");
+    const updateTodos = currentTodos.map(todo => todo.id === params.id  ? {...todo, text : newTodo } : todo);
+    localStorage.setItem("task",JSON.stringify(updateTodos));
+    return redirect("/");
+}
+function Update() {
+  const data = useLoaderData();
+  const currentTodos = JSON.parse(localStorage.getItem("task") || "[]");
+  const getDesiredTodo = currentTodos.find((el) => el.id === data);
+  const [text,setText] = useState(getDesiredTodo?.text || "");
+  return (
+    <>
+      <Form method="post" className="flex space-x-3 mt-10">
+        <input
+          type="text"
+          name="task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="h-10 rounded-md border w-full px-3 outline-none focus:ring-1"
+        />
+
+        <input
+          type="submit"
+          value="Update"
+          className="flex items-center justify-center rounded-md p-2 w-20 bg-blue-950 text-[#E1D9D1] font-semibold hover:opacity-90 transition"
+        />
+      </Form>
+    </>
+  );
+}
+export default Update;
